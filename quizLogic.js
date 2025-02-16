386999d1c0bb1a4de50718db5df89d6c2f374e53
yakugaku22 <haru.aka.aka@gmail.com>
2025-02-16 16:10:33 +0900

    Update quizLogic.js

--- quizLogic.js
+++ quizLogic.js
@@ -1,8 +1,3 @@
-fetch("./quizData.json")
-  .then(response => response.json())
-  .then(data => console.log("データ取得成功！", data))
-  .catch(error => console.error("データ取得失敗:", error));
-
 let quizData = [];
 let currentQuestion = 0;
 
